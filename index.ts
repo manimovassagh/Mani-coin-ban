d0ea5769453a95cad39a3b97cafbf4cc6e559c05
Mani Movassagh <mani.mgh@gmail.com>
2023-09-26 21:18:18 +0200

    final

--- index.ts
+++ index.ts
@@ -3,9 +3,6 @@ import { blockChainFactory } from './data/blockchain';
 import { Blockchain } from "./models/Blockchain";
 
 
-const chain: Blockchain = blockChainFactory()
-
-console.log(chain.viewDTO());
 
 
 
